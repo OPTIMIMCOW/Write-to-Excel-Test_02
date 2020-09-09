7bf9ff4066e022bd01ad38349df30cdff08f1692
OPTIMIMCOW <kenallan18@hotmail.com>
2020-09-09 15:43:41 +0100

    Exercise 5 Code

--- Write to Excel Test_02/Program.cs
+++ Write to Excel Test_02/Program.cs
@@ -23,30 +23,48 @@ namespace Write_to_Excel_Test_02
                 oXL.Visible = false;
 
                 //Get a new workbook.
-                oWB = (Microsoft.Office.Interop.Excel._Workbook)(oXL.Workbooks.Add(@"C:\Users\kallan\OneDrive - Thornton Tomasetti, Inc-\RnD\Carbon Calculator\Research\Programming\GH Excel Interface\Visual Studio\Example3.xlsx"));
+                oWB = (Microsoft.Office.Interop.Excel._Workbook)(oXL.Workbooks.Add(@"C:\Users\kallan\OneDrive - Thornton Tomasetti, Inc-\RnD\Carbon Calculator\Research\Programming\GH Excel Interface\Visual Studio\Example5.xlsx"));
                 oSheet = (Microsoft.Office.Interop.Excel._Worksheet)oWB.ActiveSheet;
 
-                //Add table headers going cell by cell.
-                oSheet.Cells[3, 2] = 3;
+                // alternative to get named range
+                var value = oSheet.get_Range("namedTable").Value;//get value of named range
+                // alter value in named range
+                //oSheet.get_Range("namedTable").Value2 = "Test"; // this works
+
+                //Microsoft.Office.Interop.Excel.Range testSubSet = oSheet.get_Range("namedTable").Rows["1:1"]; // this works
+                Microsoft.Office.Interop.Excel.Range testSubSet = oSheet.get_Range("namedTable").Cells[1,1]; // this also works
+                testSubSet.Value2 = "Updated SubSet";
+
+                //// get named table
+                //String myTableName = "namedTable";
+                //Microsoft.Office.Interop.Excel.Range myTable = oXL.Range[myTableName];
+
+                //// get value in named table
+                //Microsoft.Office.Interop.Excel.WorksheetFunction VariableObject = oXL.WorksheetFunction;
+
+                //double rowRef = 1;
+                //double colRef = 1;
+
+                //Object displayVariable = VariableObject.Index(myTable, rowRef, colRef);
 
 
-                // get value of a specific cell in the spredsheet
-                var VlookupCellValue = (String)(oSheet.Cells[3, 6] as Microsoft.Office.Interop.Excel.Range).Value;
-   
-                Console.Write(VlookupCellValue);
-                Console.ReadLine();
+                //String myString = (String) displayVariable;
 
-                // get value of a specific cell in the spredsheet
-                var matchCellValue = (String)(oSheet.Cells[3, 4] as Microsoft.Office.Interop.Excel.Range).Value;
+                ////Add table headers going cell by cell.
+                //// oSheet.Cells[3, 2] = 3;
 
-                Console.Write(matchCellValue);
-                Console.ReadLine();
+
+                //// get value of a specific cell in the spredsheet
+
+
+                //Console.Write(myString);
+                //Console.ReadLine();
 
 
 
                 oXL.Visible = false;
                 oXL.UserControl = false;
-                oWB.SaveAs(@"C:\Users\kallan\OneDrive - Thornton Tomasetti, Inc-\RnD\Carbon Calculator\Research\Programming\GH Excel Interface\Visual Studio\Example3_alter.xlsx", Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing,
+                oWB.SaveAs(@"C:\Users\kallan\OneDrive - Thornton Tomasetti, Inc-\RnD\Carbon Calculator\Research\Programming\GH Excel Interface\Visual Studio\Example5_alter.xlsx", Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing,
                     false, false, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange,
                     Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
 
